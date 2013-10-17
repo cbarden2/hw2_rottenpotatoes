beb362241d205d7e62cd1c775648c64db1e6c7aa
cbarden2 <bardec1205@gmail.com>
2013-10-17 09:48:24 -0700

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,57 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if session[:ratings] && !params[:ratings]
+      if session[:redirected] == 0 || session[:redirected].nil?
+        redirect_to movies_path session[:ratings]
+        session[:redirected] = 1
+        else
+          session[:redirected] = 0
+        end
+    end
+
+    #setting up the sort_by key in session
+    if !params[:sort_by].nil?
+      session[:sort_by] = params[:sort_by]
+    end
+    
+    if params.has_key? :ratings
+      session[:ratings] = params[:ratings]
+    elsif params.has_key? :commit
+      session[:ratings] = nil
+    end
+
+    #set up the proper variables for what movies you want
+    @sort_by = session[:sort_by]
+    @ratings = session[:ratings]
+    
+   ##pull in movies in sorted order
+    if @ratings
+      @movies = Movie.order(@sort_by).where("title != ''").select do |m|
+          @ratings.include? m.rating
+      end
+    end
+
+   ##set up the variables for the classes in the haml
+    if session[:sort_by] == 'title'
+      @title_class = 'hilite'
+    elsif session[:sort_by] == 'release_date'
+      @release_date_class = 'hilite'
+    end
+    
+    ##make an array of possible movie ratings available to the view
+    @all_ratings = Movie.possible_ratings
+    
+    @ratings = Hash.new
+    @all_ratings.each do |rating|
+      if session[:ratings].class == Hash
+        if session[:ratings].has_key? rating
+          @ratings[rating]= false
+        else
+          @ratings[rating] = true
+        end
+      end
+    end
   end
 
   def new
